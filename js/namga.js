bcae31cd1a459bf7bdf74fa68a4bc59e48ebdea7
rail_rz <zamaletdinov.rz@gmail.com>
2014-01-21 00:22:52 +0400

    отрисовка квадрата

--- js/namga.js
+++ js/namga.js
@@ -4,24 +4,17 @@
 
 // Создаем канвас
 var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
+var context = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 450;
+canvas.style.border =  "1px solid black";
 // добавляет элемент последним в список детей
 document.body.appendChild(canvas);
 
-// цикл игры
-var lastTime;
-function main() {
-	var now = Date.now();
-	var dt = (now - lastTime) / 1000.0;
+//начало отрисовки квадрата
+context.beginPath();
+//закрашенный квадрат, х y width height
+context.fillRect(100, 100, 100, 100);
+context.closePath();
 
-	// метод для обнавления изображения
-	update(dt);
-	// метод для нового рендера
-	render();
 
-	lastTime = now;
-	//используем requestAnimationFrame для постановки в очередь следующего цикла
-	requestAnimFrame(main);
-};
