a812f6f3e98580deb38293b7cd1dd10ac24a3d42
rail_rz <zamaletdinov.rz@gmail.com>
2014-01-21 00:23:27 +0400

    отрисовка шахматной доски

--- js/namga.js
+++ js/namga.js
@@ -18,3 +18,10 @@ context.fillRect(100, 100, 100, 100);
 context.closePath();
 
 
+context.strokeRect(18, 18, 260, 260);
+context.fillRect(20, 20, 256, 256);
+for (i = 0; i < 8; i += 2)
+	for (j = 0; j < 8; j += 2) {
+		context.clearRect(20 + i * 32, 20 + j * 32, 32, 32);
+		context.clearRect(20 + (i + 1) * 32, 20 + (j + 1) * 32, 32, 32);
+	}
