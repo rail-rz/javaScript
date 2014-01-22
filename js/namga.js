852b0ce5632dbc96cb87490005bee88f9827085f
rail_rz <zamaletdinov.rz@gmail.com>
2014-01-22 22:32:33 +0400

    синий квадрат

--- js/namga.js
+++ js/namga.js
@@ -13,15 +13,10 @@ document.body.appendChild(canvas);
 
 //начало отрисовки квадрата
 context.beginPath();
+//задаем цвет
+context.fillStyle = 'darkblue';
 //закрашенный квадрат, х y width height
-context.fillRect(100, 100, 100, 100);
+context.fillRect(100, 100, 50, 50);
 context.closePath();
 
 
-context.strokeRect(18, 18, 260, 260);
-context.fillRect(20, 20, 256, 256);
-for (i = 0; i < 8; i += 2)
-	for (j = 0; j < 8; j += 2) {
-		context.clearRect(20 + i * 32, 20 + j * 32, 32, 32);
-		context.clearRect(20 + (i + 1) * 32, 20 + (j + 1) * 32, 32, 32);
-	}
