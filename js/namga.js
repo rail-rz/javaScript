511eaaa2290c272dc6625d496a78865257af50ee
rail_rz <zamaletdinov.rz@gmail.com>
2014-01-25 17:23:07 +0400

    работа с изображениями

--- js/namga.js
+++ js/namga.js
@@ -7,6 +7,7 @@ var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 450;
+//canvas.style.backgroundColor = 'black';
 canvas.style.border =  "1px solid black";
 // добавляет элемент последним в список детей
 document.body.appendChild(canvas);
@@ -19,4 +20,9 @@ context.fillStyle = 'darkblue';
 context.fillRect(100, 100, 50, 50);
 context.closePath();
 
-
+//добавим картинку
+var img = new Image();
+img.src = 'image/pe_ship.png';
+img.onload = function() {    // Событие onLoad, ждём момента пока загрузится изображение
+	context.drawImage(img, 0, 0);  // Рисуем изображение от точки с координатами 0, 0
+}
