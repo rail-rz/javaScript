2cc0b23a001c91d199e3c270c55b8cae2ed9ed88
rail_rz <zamaletdinov.rz@gmail.com>
2014-05-25 16:02:43 +0400

    block move 8-05-2014 release 1

---
+++ js/prototypeInit.js
@@ -0,0 +1,20 @@
+/**
+ * @author rail_rz <zamaletdinov.rz@gmail.com>
+ */
+
+function Init() {
+    // сохраняем макс высоту и ширину
+    var heightWindow = window.innerHeight;
+    var widthWindow = window.innerWidth; 
+    
+    // сразу задаем полную нужным блокамы
+    document.getElementById('content-right').style.height = heightWindow+'px';
+    document.getElementById('content-left-workzone').style.height = sizeControl(heightWindow, 75);
+    document.getElementById('content-left-helperzone').style.height = sizeControl(heightWindow, 25);
+}
+
+//контроль за размером hundredPercentSize - 100% размер в px, percentage - процент уменьшения
+// возвращает размер в пикселах
+function sizeControl( hundredPercentSize, percentage) {
+    return (hundredPercentSize * percentage / 100) + "px";
+}
