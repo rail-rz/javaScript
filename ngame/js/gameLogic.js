5433c182e82df6a99318b99ce0f577b482d1c6d0
rail_rz <zamaletdinov.rz@gmail.com>
2015-03-18 01:39:21 +0300

    refactoring, 17-03-2015

--- ngame/js/gameLogic.js
+++ ngame/js/gameLogic.js
@@ -3,7 +3,7 @@
  * @author rail_rz <zamaletdinov.rz@gmail.com>
  */
 
-function playGame(canvas) {
+function playGame(startParams) {
 	var timer = 0,
 		Constant = new NConstant(),
 		randomBotParams = {
@@ -20,23 +20,14 @@ function playGame(canvas) {
 		},
 		factory = new NElementFactory();
 
-	var canvasParam = {  method:'rect', path:'image/background.jpg', color:'grey', realWidth:Constant.canvasWidth(), realHeight:Constant.canvasHeight(), imageWidth:800, imageHeight:600};
-	var groundParams = { method:'rect', color:'black', y:canvasParam.realHeight-20, realHeight:20, realWidth:canvasParam.realWidth};
-	var playerParams = { method:'sprite', type:'player', health:10, path:'image/nlo.png', x:canvasParam.realWidth/2 - 100/2, y:0, imageWidth:300, imageHeight:300, realWidth:100, realHeight:100, frameX:3, frameY:2, currentFrameX:0, currentFrameY:0, speedX:5, speedY:5};
-	var otherElements = [
-		{ method:'rect', type:'build', health:100, color:'black', x:canvasParam.realWidth/2 - playerParams.realWidth/2, y:canvasParam.realHeight,realHeight:playerParams.realHeight, realWidth:playerParams.realWidth, is_killed:0,
-			is_crash:1, is_event:1}
-	];
-	var gunParams = {method:'rect', color:'yellow', speedY:2, opacity:0.3, realWidth:30, is_kill:0};
-
-    this.gameCanvas = factory.createElement(canvasParam);
-	this.player = factory.createElement(playerParams);
-    this.gun = factory.createElement(gunParams);
-	this.ground = factory.createElement(groundParams);
+    this.gameCanvas = factory.createElement(startParams.canvasParam);
+	this.player = factory.createElement(startParams.playerParams);
+    this.gun = factory.createElement(startParams.gunParams);
+	this.ground = factory.createElement(startParams.groundParams);
 
 	this.elements = [];
-	for(var i = 0; i < otherElements.length; i++) {
-		this.elements[i] = factory.createElement(otherElements[i]);
+	for(var i = 0; i < startParams.otherElements.length; i++) {
+		this.elements[i] = factory.createElement(startParams.otherElements[i]);
 	}
 
     // отрисовывающая функуия
@@ -48,31 +39,7 @@ function playGame(canvas) {
 		this.ground.drawing();
 
 		this.keyboardControl();
-
-		// столкновение со стеной
-		if(WallController(this.player)) {
-			if(this.player.x < 0) {
-				this.player.x = 0;
-			} else if(this.player.x + this.player.width > this.gameCanvas.width) {
-				this.player.x = this.gameCanvas.width - this.player.width;
-			}
-			if(this.player.y <0 ) {
-				this.player.y = 0;
-			} else if(this.player.y + this.player.height > this.gameCanvas.height) {
-				this.player.y = this.gameCanvas.height - this.player.height;
-			}
-		}
-
-		if(this.player.y + this.player.height > this.gameCanvas.height - this.player.height) {
-			this.player.y = this.gameCanvas.height - 2* this.player.height;
-		}
-
-		// столкновение с землей
-		if(CrashController(this.ground, this.player)) {
-			if(this.player.y + this.player.height > this.ground.y) {
-				this.player.y = this.ground.y - this.player.height;
-			}
-		}
+		this.playerLogic();
 
 		// обработка игровых элементов
 		for(var i = 0; i < this.elements.length; i++) {
@@ -86,9 +53,6 @@ function playGame(canvas) {
 					this.elements[i].x -= this.elements[i].speedX;
 
 					if(++this.elements[i].timer%100 ==0) {
-
-						var plx = this.player.x + this.player.width/2 - this.elements[i].x;
-						var ply = this.player.y + this.player.height/2 - this.elements[i].y;
 						this.elements.push(factory.createElement(
 							{
 								method:'rect',
@@ -98,8 +62,8 @@ function playGame(canvas) {
 								y:this.elements[i].y,
 								realWidth:5,
 								realHeight:5,
-								speedX: plx/50,
-								speedY: ply/50,
+								speedX: (this.player.x + this.player.width/2 - this.elements[i].x)/50,
+								speedY: (this.player.y + this.player.height/2 - this.elements[i].y)/50,
 								is_killed:1,
 								is_kill:1,
 								is_crash:0
@@ -144,7 +108,7 @@ function playGame(canvas) {
 			}
 
 			// столкновение с оружием
-			if(CrashController(this.gun, this.elements[i]) && keysMap[32] && this.gun.height) {
+			if(CrashController(this.gun, this.elements[i])) {
 				if(this.elements[i].is_event) {
 					this.elements[i].is_event = 0;
 					if(this.elements.length <= 5) {
@@ -153,7 +117,6 @@ function playGame(canvas) {
 						this.elements.push(factory.createElement({ method:'rect', color:'green', x:(this.elements[i].x + this.elements[i].width/2), y:this.elements[i].y-20, realWidth:10, realHeight:20, speedX:-getRandomValueFromArray(randomBotParams.speedX), speedY:9.8, is_killed:1, is_crash:0}));
 					}
 				}
-
 				if(this.gun.is_kill) {
 					if(--this.elements[i].health <= 0) {
 						this.elements.splice(i, 1);
@@ -164,7 +127,6 @@ function playGame(canvas) {
 					} else {
 						this.gun.height = this.elements[i].y - this.gun.y;
 					}
-
 				} else {
 					if(this.elements[i].is_killed) {
 						this.elements[i].x -= this.elements[i].speedX;
@@ -227,11 +189,11 @@ function playGame(canvas) {
 		}
 		if(keysMap[81]) {
 			this.gun.height = 0;
-			this.gun.width = gunParams.realWidth;
-			this.gun.speedY = gunParams.speedY;
-			this.gun.is_kill = gunParams.is_kill;
-			this.gun.color = gunParams.color;
-			this.gun.opacity = gunParams.opacity;
+			this.gun.width = startParams.gunParams.realWidth;
+			this.gun.speedY = startParams.gunParams.speedY;
+			this.gun.is_kill = startParams.gunParams.is_kill;
+			this.gun.color = startParams.gunParams.color;
+			this.gun.opacity = startParams.gunParams.opacity;
 		}
 		//player move
 		if(keysMap[32]) {
@@ -261,6 +223,33 @@ function playGame(canvas) {
 
 	};
 
+	this.playerLogic = function() {
+		// столкновение со стеной
+		if(WallController(this.player)) {
+			if(this.player.x < 0) {
+				this.player.x = 0;
+			} else if(this.player.x + this.player.width > this.gameCanvas.width) {
+				this.player.x = this.gameCanvas.width - this.player.width;
+			}
+			if(this.player.y <0 ) {
+				this.player.y = 0;
+			} else if(this.player.y + this.player.height > this.gameCanvas.height) {
+				this.player.y = this.gameCanvas.height - this.player.height;
+			}
+		}
+
+		if(this.player.y + this.player.height > this.gameCanvas.height - this.player.height) {
+			this.player.y = this.gameCanvas.height - 2* this.player.height;
+		}
+
+		// столкновение с землей
+		if(CrashController(this.ground, this.player)) {
+			if(this.player.y + this.player.height > this.ground.y) {
+				this.player.y = this.ground.y - this.player.height;
+			}
+		}
+	};
+
 	this.start = function() {
 		this.setIntervalId = setInterval(this.drawingGame, 1000 / 50);
 	};
@@ -270,4 +259,20 @@ function playGame(canvas) {
 	};
 
 	this.start();
+}
+
+
+function newGame() {
+	var Constant = new NConstant(),
+		startParams = {
+			canvasParam: {  method:'image', path:'image/background.jpg', color:'grey', realWidth:Constant.canvasWidth(), realHeight:Constant.canvasHeight(), imageWidth:800, imageHeight:600},
+			groundParams: { method:'rect', color:'black', y:Constant.canvasHeight()-20, realHeight:20, realWidth:Constant.canvasWidth()},
+			playerParams: { method:'sprite', type:'player', health:10, path:'image/nlo.png', x:Constant.canvasWidth()/2 - 100/2, y:0, imageWidth:300, imageHeight:300, realWidth:100, realHeight:100, frameX:3, frameY:2, currentFrameX:0, currentFrameY:0, speedX:5, speedY:5},
+			otherElements: [
+				{ method:'rect', type:'build', health:100, color:'black', x:Constant.canvasWidth()/2 - 100/2, y:Constant.canvasWidth(),realHeight:100, realWidth:100, is_killed:0, is_crash:1, is_event:1}
+			],
+			gunParams: {method:'rect', color:'yellow', speedY:2, opacity:0.3, realWidth:30, is_kill:0}
+		};
+
+	playGame(startParams);
 }
--- ngame/js/init.js
+++ ngame/js/init.js
@@ -7,19 +7,19 @@ function init() {
     var canvas = document.getElementById('namga-game');
 	// инициализируем контекст для фабрики
 	NElement.context = canvas.getContext("2d");
-	var constant = new NConstant();
-	canvas.style.width = constant.canvasWidth() + 'px';
-	canvas.style.height = constant.canvasHeight() + 'px';
+	var Constants = new NConstant();
+	canvas.style.width = Constants.canvasWidth() + 'px';
+	canvas.style.height = Constants.canvasHeight() + 'px';
 	// задаем размеры и разрешение canvas
-	canvas.width = constant.canvasWidth();
-	canvas.height = constant.canvasHeight();
+	canvas.width = Constants.canvasWidth();
+	canvas.height = Constants.canvasHeight();
 
     // функции слушатили
 //  canvas.onmousemove = mouseMove;
     window.addEventListener( "keydown", doKeyDown, false);
     window.addEventListener( "keyup", doKeyUp, false);
 
-	delete constant;
-    playGame(canvas);
+	delete Constants;
+    newGame();
 
 }
