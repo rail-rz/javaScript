653f83eb31a5837184bcdfd6e2c59c69f5f3aa06
rail-rz <zamaletdinov@sports.ru>
2015-09-28 19:54:46 +0300

    star working under view

---
+++ greeting_card/js/init-view.js
@@ -0,0 +1,29 @@
+/**
+ * Инициализирующая проект функция
+ * @author rail_rz <zamaletdinov.rz@gmail.com>
+ */
+
+function init() {
+    var Constants = new NConstant(),
+        canvas = document.getElementById('namga-game');
+
+    // инициализируем контекст для фабрики
+    NElement.context = canvas.getContext("2d");
+    canvas.style.width = Constants.canvasWidth() + 'px';
+    canvas.style.height = Constants.canvasHeight() + 'px';
+    canvas.style.top = canvas.offsetTop + 'px';
+    canvas.style.left = canvas.offsetLeft + 'px';
+    // задаем размеры и разрешение canvas
+    canvas.width = Constants.canvasWidth();
+    canvas.height = Constants.canvasHeight();
+
+    // функции слушатили
+//  canvas.onmousemove = mouseMove;
+//    window.addEventListener( "keydown", doKeyDown, false);
+//    window.addEventListener( "keyup", doKeyUp, false);
+
+    playGame();
+    // времнное решение
+    startGame();
+    stopGame();
+}
---
+++ greeting_card/js/move-logic.js
@@ -0,0 +1,81 @@
+/**
+ * Логика игры
+ * @author rail_rz <zamaletdinov.rz@gmail.com>
+ */
+
+function playGame() {
+	var setIntervalId = null,
+		Constant = new NConstant(),
+		startParams = {},
+		factory = new NElementFactory(),
+		gameCanvas,
+		elements,
+		drawingGame;
+
+    // отрисовывающая функуия
+    drawingGame = function() {
+        gameCanvas.drawing();
+
+       	// обработка игровых элементов
+		for(var i = 0; i < elements.length; i++) {
+			elements[i].update();
+			elements[i].drawing();
+
+			// логика при столкновении с элементами
+			/*if(elements[i].is_crash ) {
+				var crash = penetration(player, elements[i]);
+				if(crash){
+					if(crash.x <= player.speedX) {
+						if(crash.direction.x <0) {
+							player.x -= crash.x;
+						} else if(crash.direction.x > 0) {
+							player.x += crash.x;
+						}
+					}
+					if(crash.y <= player.speedY) {
+						if(crash.direction.y < 0) {
+							player.y -= crash.y;
+						} else if(crash.direction.y > 0) {
+							player.y += crash.y;
+						}
+					}
+				}
+			}*/
+
+		}
+	};
+
+
+	this.startGame = function() {
+		this.stopGame();
+		startParams = {
+			canvasParam: {  method:'image', path:'image/background.jpg', color:'grey', realWidth:Constant.canvasWidth(), realHeight:Constant.canvasHeight(), imageWidth:800, imageHeight:600},
+			otherElements: [
+				{ method:'rect', type:'build', health:100, color:'black', x:Constant.canvasWidth()/2 - 100/2, y:Constant.canvasWidth(),realHeight:100, realWidth:100, is_killed:0, is_crash:1, is_event:1}
+			]
+		};
+		gameCanvas = factory.createElement(startParams.canvasParam);
+
+		elements = [];
+		for(var i = 0; i < startParams.otherElements.length; i++) {
+			elements[i] = factory.createElement(startParams.otherElements[i]);
+		}
+		setIntervalId = setInterval(drawingGame, 1000 / 50);
+
+		return true;
+	};
+
+	this.continueGame = function() {
+		this.stopGame();
+        setIntervalId = setInterval(drawingGame, 1000 / 50);
+			return true;
+	};
+
+	this.stopGame = function() {
+		if(setIntervalId != null ) {
+			clearInterval(setIntervalId);
+		}
+		return true;
+	};
+
+}
