6a11680e29d85e62c26c088f5219b6957c96d557
rail-rz <zamaletdinov@sports.ru>
2015-10-28 12:05:17 +0300

    candel test

--- greeting_card/js/move-logic.js
+++ greeting_card/js/move-logic.js
@@ -28,11 +28,12 @@ function playGame() {
 		this.stopGame();
 		startParams = {
 			otherElements: [
-				{ method:'rect',   type:'div', color:'red',   left:2,   top:100, width:100, height:100, speedX:4, speedRotate:1},
+				/*{ method:'rect',   type:'div', color:'red',   left:2,   top:100, width:100, height:100, speedX:4, speedRotate:1},
                 { method:'rect',   type:'div', color:'blue',  left:500, top:0,   width:100, height:100, speedY:4, opacityMin:0.1, opacityMax:0.9, opacity:1 },
                 { method:'rect',   type:'div', color:'green', left:0,   top:0,   width:100, height:100, speedY:5, speedX:5, rotate:40},
-                { method:'image',  type:'img', path:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQL82tckOtHxX4LwHGEFCJ1OxL2JcHWCrJ_FMF8La0FLFlqw-h2cgn2KyZj', left:50, top:50, width:100, height:100, speedY:-3, speedX:-2, opacityMin:0.3, opacityMax:0.5},
-                { method:'sprite', type:'div', path:'images/nlo.png', left:700, top:500, width:100, height:100, speedY:-1, speedX:2, frameX:4, frameY:3, currentFrameX:0,currentFrameY:0, rotate:40, opacityMin:0, opacityMax:1, opacitySpeed:0.05}
+                { method:'image',  type:'img', path:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQL82tckOtHxX4LwHGEFCJ1OxL2JcHWCrJ_FMF8La0FLFlqw-h2cgn2KyZj', left:50, top:50, width:100, height:100, speedY:-3, speedX:-2, opacityMin:0.3, opacityMax:0.5},*/
+                { method:'rect',   type:'div', color:'#ffffff', left:300, top:390, width:200, height:190},
+                { method:'sprite', type:'div', path:'images/candel_relise.png', left:325, top:100, width:150, height:300, frameX:7, frameY:3, currentFrameX:0,currentFrameY:0, opacityMin:0.4, opacityMax:0.7, opacitySpeed:0.01}
 			]
 		};
 
